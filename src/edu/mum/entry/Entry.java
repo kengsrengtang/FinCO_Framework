7abc5d7c8f0b7a86784a999bb83b5f452acf1796
Bikram <magar.bik@gmail.com>
2013-11-17 10:04:03 -0600

    Interface of Entry

---
+++ src/edu/mum/entry/Entry.java
@@ -0,0 +1,20 @@
+package edu.mum.entry;
+
+import java.util.Date;
+
+public class Entry implements IEntry{
+	Date date;
+	Double amount;
+	@Override
+	public void deposit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void withdraw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
