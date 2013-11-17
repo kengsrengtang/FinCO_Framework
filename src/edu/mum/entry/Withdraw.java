ec05955c7f045421b1835e6f11773718c0b1ef35
Bikram <magar.bik@gmail.com>
2013-11-17 15:03:25 -0600

    added constructor for withdraw and deposit operation

--- src/edu/mum/entry/Withdraw.java
+++ src/edu/mum/entry/Withdraw.java
@@ -4,8 +4,15 @@ import edu.mum.account.IAccount;
 
 public class Withdraw implements IOperation{
 
-	IAccount account;
-	IEntry entry;
+	private IAccount account;
+	private IEntry entry;
+	
+	public Withdraw(IAccount account, IEntry entry) {
+		super();
+		this.account = account;
+		this.entry = entry;
+	}
+
 	@Override
 	public void execute() {
 		// TODO Auto-generated method stub
