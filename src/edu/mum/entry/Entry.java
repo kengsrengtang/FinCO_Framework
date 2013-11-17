f011f0d5431baeea70e62121d080a1c1b59993fb
Bikram <magar.bik@gmail.com>
2013-11-17 14:57:00 -0600

    added consturctor

--- src/edu/mum/entry/Entry.java
+++ src/edu/mum/entry/Entry.java
@@ -5,6 +5,11 @@ import java.util.Date;
 public class Entry implements IEntry{
 	Date date;
 	double amount;
+	public Entry(Date date, double amount) {
+		super();
+		this.date = date;
+		this.amount = amount;
+	}
 	@Override
 	public double getAmount() {
 		// TODO Auto-generated method stub
