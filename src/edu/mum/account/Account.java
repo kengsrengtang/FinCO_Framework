165fbe468d353d29048b880858908404900b0d34
mhawila <a.mhawila@gmail.com>
2013-11-17 10:41:30 -0600

    Account package phase I completed

--- src/edu/mum/account/Account.java
+++ src/edu/mum/account/Account.java
@@ -3,23 +3,26 @@ package edu.mum.account;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.mum.client.IClient;
 import edu.mum.entry.IEntry;
 
 public class Account implements IAccount {
 	protected List<IEntry> entries = new ArrayList<IEntry>();
 	private String accountNo;
 	private double balance;
+	private IClient client;
 	
-	public Account(String accountNo) {
+	public Account(IClient client,String accountNo) {
+		this.client = client;
 		this.accountNo = accountNo;
 	}
 
 	public String getAccountNo() {
 		return accountNo;
 	}
-
-	public void setAccountNo(String accountNo) {
-		this.accountNo = accountNo;
+	
+	public IClient getClient() {
+		return client;
 	}
 
 	public void setBalance(double balance) {
@@ -38,7 +41,7 @@ public class Account implements IAccount {
 	}
 
 	@Override
-	public void notifyCustomer() {
+	public void notifyClient() {
 		// TODO Auto-generated method stub
 	}
 
--- src/edu/mum/account/IAccount.java
+++ src/edu/mum/account/IAccount.java
@@ -1,9 +1,11 @@
 package edu.mum.account;
 
+import edu.mum.client.IClient;
 import edu.mum.entry.IEntry;
 
 public interface IAccount {
+	IClient getClient();
 	double getBalance();
 	void addEntry(IEntry entry);
-	void notifyCustomer();
+	void notifyClient();
 }
