4325020c8b1ef4d23639ebc486e61e23d51785cd
Kengsreng Tang <kengsrengkh@gmail.com>
2013-11-18 16:23:50 -0600

    added interestRate to Bank Application

--- src/edu/mum/bank/CheckingAccount.java
+++ src/edu/mum/bank/CheckingAccount.java
@@ -3,15 +3,23 @@ package edu.mum.bank;
 import edu.mum.client.IClient;
 
 public class CheckingAccount extends BankAccount{
-
+	private double interestRate;
 	public CheckingAccount(IClient client, String accountNo) {
 		super(client, accountNo);
-		// TODO Auto-generated constructor stub
+		this.interestRate = 0.01;
 	}
 
 	@Override
 	void addInterest(double amount) {
 		this.setBalance(amount);
+	}
+
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
 	}
 
 }
--- src/edu/mum/bank/SavingAccount.java
+++ src/edu/mum/bank/SavingAccount.java
@@ -3,10 +3,19 @@ package edu.mum.bank;
 import edu.mum.client.IClient;
 
 public class SavingAccount extends BankAccount{
+	private double interestRate;
+	
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	public void setInterestRate(double interestRate) {
+		this.interestRate = interestRate;
+	}
 
 	public SavingAccount(IClient client, String accountNo) {
 		super(client, accountNo);
-		// TODO Auto-generated constructor stub
+		this.interestRate = 0.02;
 	}
 
 	@Override
